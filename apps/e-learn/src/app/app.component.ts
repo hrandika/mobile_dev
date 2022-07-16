8d577da4df51008879cb3dab2c75d035995d3d93
Randika Hapugoda <randika@Randikas-MacBook-Pro.local>
2022-07-16 09:12:17 +0530

    initial commit

---
+++ apps/e-learn/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'responsive-inc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'e-learn';
+}
