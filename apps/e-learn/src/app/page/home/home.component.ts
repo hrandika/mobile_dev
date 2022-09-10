c4eaceadeb088eb8b1346aeeeecd6e14beb68837
hrandika <hrandika@hotmail.com>
2022-09-10 09:15:30 +0530

    interval subs

--- apps/e-learn/src/app/page/home/home.component.ts
+++ apps/e-learn/src/app/page/home/home.component.ts
@@ -1,43 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { filter, interval, map, of, Subscription, take, tap } from 'rxjs';
 
 interface CardDetails {
   title: string;
 }
-
 
 @Component({
   selector: 'responsive-inc-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   cards: CardDetails[] = [
-    {
-      title: 'Randika',
-    },
-    {
-      title: 'Chanaka',
-    },
-    {
-      title: 'Padma',
-    },
-    {
-      title: 'Supuni',
-    },
-    {
-      title: 'Randika',
-    },
-    {
-      title: 'Chanaka',
-    },
-    {
-      title: 'Padma',
-    },
-    {
-      title: 'Supuni',
-    },
+    { title: 'Randika' },
+    { title: 'Chanaka' },
+    { title: 'Padma' },
+    { title: 'Supuni' },
+    { title: 'Randika' },
+    { title: 'Chanaka' },
+    { title: 'Padma' },
+    { title: 'Supuni' },
   ];
-  constructor() {}
 
-  ngOnInit(): void {}
+  public count = 0;
+  private subcsription: Subscription;
+
+  constructor() {
+    //
+    of(this.cards).pipe(
+      tap((result) => console.log(result)),
+      map((x) => {
+        const newArrya = [...x];
+        newArrya.pop();
+        return newArrya;
+      }),
+      tap((result) => console.log(result))
+    );
+    // .subscribe();
+
+    this.subcsription = interval(500)
+      .pipe(
+        tap((v) => console.log(v)),
+        filter((val) => val % 2 !== 0),
+        // tap((v) => console.log(v))
+        tap((v) => (this.count = v))
+      )
+      .subscribe();
+  }
+
+  ngOnInit(): void {
+    console.log();
+  }
+
+  ngOnDestroy(): void {
+    this.subcsription.unsubscribe();
+  }
 }
